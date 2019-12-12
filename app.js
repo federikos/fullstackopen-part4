6cdb5d1cc28a326e5b67bbd25df6d8a474e97d5b
federikos <federikos@bk.ru>
2019-12-12 15:47:37 +0300

    4.2 Blog list, step2

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs');
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
