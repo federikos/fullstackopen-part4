9dcbe697dffe64068785e7192f52679eaef66854
federikos <federikos@bk.ru>
2019-12-12 20:50:57 +0300

    complete exercise #4.3: helper functions and unit tests, step1

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
-const config = require('./utils/config')
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require('dotenv').config();
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message);
+    });
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.use('/api/blogs', blogsRouter);
 
