d07cce86d309b6de33df854d978b3247113e53c9
federikos <federikos@bk.ru>
2022-05-09 01:35:53 +0300

    feat: 4.17: bloglist expansion, step5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 });
   response.json(blogs);
 });
 
@@ -11,13 +13,19 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end();
   }
 
+  const user = await User.findOne({});
+
   const blog = new Blog({
     ...request.body,
-    likes: request.body.likes || 0
+    likes: request.body.likes || 0,
+    user: user._id
   });
 
-  const result = await blog.save();
-  response.status(201).json(result);
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
