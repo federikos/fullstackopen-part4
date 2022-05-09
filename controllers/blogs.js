e7956ad6dd7c121c26d8d128f319057205ec1280
federikos <federikos@bk.ru>
2022-05-09 14:44:00 +0300

    feat: 4.19: bloglist expansion, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.split(' ')[1];
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -9,11 +18,17 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  if(!request.body.title && !request.body.url) {
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  if (!request.body.title && !request.body.url) {
     return response.status(400).end();
   }
 
-  const user = await User.findOne({});
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     ...request.body,
